5a980faa70227d95ebfd037a43f2e8268048873b
loic calvy <loic@ubuntu.innovat.io.loic.calvy>
2014-03-10 23:53:54 +0100

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,20 +88,83 @@ module.exports = function (grunt) {
                 ui: 'bdd',
                 reporter: 'spec'
             }
+        },
+        concurrent: {
+          dev: {
+            tasks: ['shell:mongod', 'nodemon', 'watch'],
+            options: {
+              logConcurrentOutput: true
+            }
+          }
+        },
+        nodemon: {
+          dev: {
+            script: 'index.js',
+            options: {
+              nodeArgs: ['--debug'],
+              env: {
+                PORT: '5455'
+              },
+              // omit this property if you aren't serving HTML files and 
+              // don't want to open a browser tab on start
+              callback: function (nodemon) {
+                nodemon.on('log', function (event) {
+                  console.log(event.colour);
+                });
+
+                // opens browser on initial server start
+                nodemon.on('config:update', function () {
+                  // Delay before server listens on port
+                  setTimeout(function() {
+                    require('open')('http://localhost:8000');
+                  }, 1000);
+                });
+
+                // refreshes browser when server reboots
+                nodemon.on('restart', function () {
+                  // Delay before server listens on port
+                  setTimeout(function() {
+                    require('fs').writeFileSync('.rebooted', 'rebooted');
+                  }, 1000);
+                });
+              }
+            }
+          }
+        },
+        watch: {
+          server: {
+            files: ['.rebooted'],
+            options: {
+              livereload: true
+            }
+          } 
+        },
+        shell: {                                // Task
+            mongod: {                      // Target
+                options: {                      // Options
+                    stdout: true
+                },
+                command: 'mongod --smallfiles'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-nodemon');
     
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-cli');
     grunt.loadNpmTasks('grunt-dustjs');
     grunt.loadNpmTasks('grunt-copy-to');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shell');
     grunt.loadTasks('./node_modules/makara/tasks/');
 
     grunt.registerTask('i18n', ['clean', 'makara', 'dustjs', 'clean:tmp']);
     grunt.registerTask('build', ['jshint', 'less',  'copyto', 'i18n']);
     grunt.registerTask('test', ['jshint', 'mochacli']);
+    grunt.registerTask('default', ['concurrent']);
 
 };
